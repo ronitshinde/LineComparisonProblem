7a4cb5e8c25c5674cb07250781c1448ba5fb3cbd
ronitshinde <rony.shinde753@gmail.com>
2023-02-05 18:25:54 +0530

    [Ronit] Add : UC2-Equality Of Lines

---
+++ LineComaprisonProblem/LineComaprisonProblem/EqualityOfLines.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LineComaprisonProblem
+{
+    public class EqualityOfLines
+    {
+        int lengthX,_lengthY,lenGthsX,lengthsY;
+        public EqualityOfLines(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double CalculateOne()
+        {
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine("Lenght of the First line is : " +length);
+            return length;
+        }
+        public double CalculateTwo()
+        {
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine("Lenght of the Second line is : " + length);
+            return length;
+        }
+    }
+}
--- LineComaprisonProblem/LineComaprisonProblem/Program.cs
+++ LineComaprisonProblem/LineComaprisonProblem/Program.cs
@@ -6,10 +6,18 @@ namespace LineComaprisonProblem
     {
         static void Main(string[] args)
         {
-            CalculateLength length = new CalculateLength(4, 8, 3, 5);
-            length.Calculate();
-                                    
-            Console.ReadLine(); 
+            EqualityOfLines length_One = new EqualityOfLines(4, 2, 7, 3);
+            double length = length_One.CalculateOne();
+
+            EqualityOfLines length_Two = new EqualityOfLines(5, 8, 4, 6);
+            double length_ = length_Two.CalculateTwo();
+
+            if (length.Equals(length_))
+                Console.WriteLine("Both line are equal");
+            else
+                Console.WriteLine("Both lines are not equal");
+
+            Console.ReadLine();
         }
     }
 }
