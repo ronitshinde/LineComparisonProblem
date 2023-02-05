ab34724ffc80a5ecfcd75dce0ae0ab15d6ba8ddc
ronitshinde <rony.shinde753@gmail.com>
2023-02-05 17:19:14 +0530

    [Ronit] Add : UC1-Length of line

---
+++ LineComaprisonProblem/LineComaprisonProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComaprisonProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLength length = new CalculateLength(4, 8, 3, 5);
+            length.Calculate();
+                                    
+            Console.ReadLine(); 
+        }
+    }
+}
