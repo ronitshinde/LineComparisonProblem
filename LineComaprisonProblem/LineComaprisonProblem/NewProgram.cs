513a81134b5e5f77624075957b3c017338b18ef6
ronitshinde <rony.shinde753@gmail.com>
2023-02-15 14:25:55 +0530

    [Ronit] Add : UC4-Refactored Code

---
+++ LineComaprisonProblem/LineComaprisonProblem/NewProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComaprisonProblem
+{
+    public class NewProgram
+    {
+        public void Compare()
+        {
+            CompareTwoLines length_One = new CompareTwoLines(7, 6, 4, 2);
+            double length = length_One.CalculateOne();
+
+            CompareTwoLines length_Two = new CompareTwoLines(4, 9, 2, 1);
+            double length_ = length_Two.CalculateTwo();
+
+            if (length.CompareTo(length_) == 0)
+                Console.WriteLine("Both lines are equal ");
+            else if (length.CompareTo(length_) > 0)
+                Console.WriteLine("Line one is greater");
+            else
+                Console.WriteLine("Line two is greater");
+            Console.ReadLine();
+        }
+    }
+}
--- LineComaprisonProblem/LineComaprisonProblem/Program.cs
+++ LineComaprisonProblem/LineComaprisonProblem/Program.cs
@@ -6,19 +6,8 @@ namespace LineComaprisonProblem
     {
         static void Main(string[] args)
         {
-            CompareTwoLines length_One = new CompareTwoLines(4, 2, 7, 3);
-            double length = length_One.CalculateOne();
-
-            CompareTwoLines length_Two = new CompareTwoLines(5, 4, 5, 6);
-            double length_ = length_Two.CalculateTwo();
-
-            if (length.CompareTo(length_) == 0)
-                Console.WriteLine("Both lines are equal ");
-            else if (length.CompareTo(length_) > 0)
-                Console.WriteLine("Line one is greater");
-            else
-                Console.WriteLine("Line two is greater");
-            Console.ReadLine();
+           NewProgram newObj = new NewProgram();
+           newObj.Compare();
         }
     }
 }
